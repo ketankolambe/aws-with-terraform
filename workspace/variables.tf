8c796273d94b34191bba652d0b2c137a5c732cd9
ketan kolambe <ketannkolambe104@gmail.com>
2026-09-10 23:41:18 +0530

    workspace change

--- workspace/variables.tf
+++ workspace/variables.tf
@@ -1,16 +1,16 @@
 variable "ami_id" {
   type = string
- # default = "ami-090d68841c2a28756"
+  default = "ami-090d68841c2a28756"
 }
 
 variable "subnet_id"{
     type = string
-   # default = "subnet-04f7f972685ea3189"
+    default = "subnet-04f7f972685ea3189"
 }
 
 variable "instance_type" {
     type = string
-   # default = "t3.micro"
+    #default = "t3.micro"
 }
 
 variable "instance_count" {
