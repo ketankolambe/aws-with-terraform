b19326f6df0d4f93be8876b91dd61f3f9665e45a
ketan kolambe <ketannkolambe104@gmail.com>
2026-09-16 19:14:27 +0530

    vpc first day vpc + subnet + rt + rt_ass + igw

---
+++ environments/networking/vpc/dev/variables.tf
@@ -0,0 +1,73 @@
+variable "vpc_name" {
+  type = string
+}
+
+variable "Environment" {
+ type = string
+}
+
+variable "vpc_tag" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+
+variable "public_subnet_01_cidr" {
+  type = string
+}
+
+variable "public_subnet_01_name" {
+  type = string
+}
+
+variable "public_subnet_02_cidr" {
+  type = string
+}
+
+variable "public_subnet_02_name" {
+  type = string
+}
+
+variable "public_subnet_03_cidr" {
+  type = string
+}
+
+variable "public_subnet_03_name" {
+  type = string
+}
+
+variable "private_subnet_01_cidr" {
+  type = string
+}
+
+variable "private_subnet_01_name" {
+  type = string
+}
+
+variable "private_subnet_02_cidr" {
+  type = string
+}
+
+variable "private_subnet_02_name" {
+  type = string
+}
+
+variable "private_subnet_03_cidr" {
+  type = string
+}
+
+variable "private_subnet_03_name" {
+  type = string
+}
+
+variable "igw_name" {
+  type = string
+}
+
+variable "public_rt_name" {
+  type = string
+}
+
